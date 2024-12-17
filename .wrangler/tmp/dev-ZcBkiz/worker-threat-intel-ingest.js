ce58e787b2d2eb6f8de1ae8ac77306d381979c7f
bepsight <baranerdogan@hotmail.com>
2024-12-17 09:27:31 +0300

    feat: add middleware loader and URL checking for fetch requests to handle custom HTTPS ports

--- .wrangler/tmp/dev-ZcBkiz/worker-threat-intel-ingest.js
+++ .wrangler/tmp/dev-ZcBkiz/worker-threat-intel-ingest.js
@@ -8,7 +8,7 @@ var __require = /* @__PURE__ */ ((x) => typeof require !== "undefined" ? require
   throw new Error('Dynamic require of "' + x + '" is not supported');
 });
 
-// .wrangler/tmp/bundle-XoCYlu/checked-fetch.js
+// .wrangler/tmp/bundle-hTwk8c/checked-fetch.js
 var urls = /* @__PURE__ */ new Set();
 function checkURL(request, init) {
   const url = request instanceof URL ? request : new URL(
@@ -1648,7 +1648,7 @@ var jsonError = /* @__PURE__ */ __name(async (request, env, _ctx, middlewareCtx)
 }, "jsonError");
 var middleware_miniflare3_json_error_default = jsonError;
 
-// .wrangler/tmp/bundle-XoCYlu/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-hTwk8c/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__ = [
   middleware_ensure_req_body_drained_default,
   middleware_miniflare3_json_error_default
@@ -1680,7 +1680,7 @@ function __facade_invoke__(request, env, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-XoCYlu/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-hTwk8c/middleware-loader.entry.ts
 var __Facade_ScheduledController__ = class {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
